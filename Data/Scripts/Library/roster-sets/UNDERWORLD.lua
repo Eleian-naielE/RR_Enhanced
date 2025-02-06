16537945b2d8615dbc8a937e67ca27c588806ddd
SFGeekFromK <swjacklee@gmail.com>
2025-02-06 18:00:59 +0900

    boarding shuttle for mando added

---
+++ Data/Scripts/Library/roster-sets/UNDERWORLD.lua
@@ -0,0 +1,15 @@
+return {
+    ["VERGEBUIR"] = 0,
+    ["AKAANAR_CORVETTE"] = 0,
+	["SHAADLAR_BASILISKAN"] = 0,
+	["SHAADLAR_MANDALORIAN"] = 0,
+	["SHUKALAR_FRIGATE"] = 0,
+    ["JEHAVEYIR"] = 0,
+    ["BAARUR"] = 0,
+    ["BAARUR_MINELAYER"] = 0,
+    ["RUSUR"] = 0,
+	["KYRAMUD"] = 0,
+    ["KANDOSII"] = 0,
+    -- RR_Enhanced
+    ["MANDO_BOARDING_SHUTTLE"] = 0,
+}
