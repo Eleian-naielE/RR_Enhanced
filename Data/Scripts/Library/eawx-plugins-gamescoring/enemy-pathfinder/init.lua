6dc171a830ebdb8773b63589688a210554389d87
SFGeekFromK <swjacklee@gmail.com>
2025-04-12 09:18:30 +0900

    green7z ver bug fix 1

--- Data/Scripts/Library/eawx-plugins-gamescoring/enemy-pathfinder/init.lua
+++ Data/Scripts/Library/eawx-plugins-gamescoring/enemy-pathfinder/init.lua
@@ -4,7 +4,7 @@ return {
     type = "plugin",
     target = PluginTargets.always(),
     init = function(self, ctx)
-        EnemyPathfinder = require("eawx-plugins-gameobject-space/enemy-pathfinder/EnemyPathfinder")
+        EnemyPathfinder = require("eawx-plugins-gamescoring/enemy-pathfinder/EnemyPathfinder")
         return EnemyPathfinder()
     end
 }
