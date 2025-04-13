df4d73019071ec3effd172c09dc12a0eb28d09ef
SFGeekFromK <swjacklee@gmail.com>
2025-04-13 19:29:36 +0900

    greene's fix

--- Data/Scripts/Library/eawx-plugins-gamescoring/enemy-pathfinder/EnemyPathfinder.lua
+++ Data/Scripts/Library/eawx-plugins-gamescoring/enemy-pathfinder/EnemyPathfinder.lua
@@ -1,9 +1,8 @@
 require("PGBase")
-require("PGSpawnUnits")
 require("deepcore/std/class")
 require("deepcore/crossplot/crossplot")
 require("eawx-util/StoryUtil")
-
+require("PGSpawnUnits")
 
 EnemyPathfinder = class()
 
@@ -26,20 +25,19 @@ function EnemyPathfinder:mode_start(mode)
     if player_attacker ~= self.human_player then
         self.pathfinder_enabled = true
         self.player_enemy = player_attacker
-        self.pathfinder_done = false
     end
 end
 
 function EnemyPathfinder:update() 
     
-    if self.pathfinder_enabled == false or self.pathfinder_done then
+    if self.pathfinder_enabled == false then
         return
     end
     self:spawn_pathfinder()
 end
 
 function EnemyPathfinder:spawn_pathfinder()
-    
+	
     local spawned_list = Find_All_Objects_Of_Type("Transport | Gunship | Corvette | Frigate | Capital | SuperCapital", self.player_enemy)
     for _, spawned_unit in pairs(spawned_list) do
         spawned_unit.Hide(true)		--Hides objects
@@ -66,7 +64,7 @@ function EnemyPathfinder:spawn_pathfinder()
 
         for _, category in ipairs(category_table) do							--Loop through categories
             pathfinder_category_table = Find_All_Objects_Of_Type(category, self.player_enemy) --Search individual categories for units
-            if pathfinder_category_table > 0 then								--If table contains units
+			if table.getn(pathfinder_category_table) > 0 then								--If table contains units
                 unit_to_pathfind = pathfinder_category_table[1]					--Assign first object in table as valid pathfinder
                 unit_to_pathfind.Teleport(entry_pos_obj)						--Move pathfinder to location
                 unit_to_pathfind.Cinematic_Hyperspace_In(1)						--Do hyperspace jump
@@ -75,6 +73,7 @@ function EnemyPathfinder:spawn_pathfinder()
                 unit_to_pathfind.Make_Invulnerable(false)						--Allow damage
                 unit_to_pathfind.Prevent_AI_Usage(false)						--AI can use
                 pathfinder_id = unit_to_pathfind.Get_Parent_Mode_Object_ID()	--Store pathfinder ID
+				break
             end
             StoryUtil.ShowScreenText("Unit not found in category: "..category..", attempting next category", 5)	--debug print if unit not found in category 
         end
@@ -83,19 +82,19 @@ function EnemyPathfinder:spawn_pathfinder()
     end
 
     StoryUtil.ShowScreenText("Pathfinder ID:  "..pathfinder_id, 5)						--Debug print of pathfinder ID
-
+	
     for _, spawned_unit in pairs(spawned_list) do									--Loop once more through all units
         if spawned_unit.Get_Parent_Mode_Object_ID() ~= pathfinder_id then				--Compare ID does not match pathfinders
-            Add_Reinforcement(spawned_unit, self.player_enemy)		--Add to reinforcement pool
+            Add_Reinforcement(spawned_unit.Get_Type(), self.player_enemy)		--Add to reinforcement pool
             spawned_unit.Despawn()														--Clear unit from battle
         end
     end
-    self.pathfinder_done = true
+	
 end
 
 
 function EnemyPathfinder:mode_end()
     self.pathfinder_enabled = false
-    self.player_enemy = nil
+	StoryUtil.ShowScreenText("Script end", 5)						--Debug script mode end
 end
 return EnemyPathfinder
